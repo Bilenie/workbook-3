56923774c942af81b0eeb08ccd0b768977c11e0f
Bilenie Tilahun <bilumekb@gmail.com>
2025-04-27 23:00:44 -0500

    update my code and comments.

--- SearchInventory/src/main/java/com/pluralsight/StoreApp.java
+++ SearchInventory/src/main/java/com/pluralsight/StoreApp.java
@@ -10,39 +10,36 @@ public class StoreApp {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        //Create empty list called inventory to put our product.
+        //Create empty list called inventory to put our product and set it to our getInventory function.
         ArrayList<Product> inventory = getInventory();
-        Scanner scanner = new Scanner(System.in);
+        //Display this to the users
         System.out.println("We carry the following inventory: ");
+        //list of product will be shown by using the loop to access each product from the arraylist.
         for (int i = 0; i < inventory.size(); i++) {
             Product p = inventory.get(i);
+            //%n USE THIS TO PUT NEXT LINE=>display the product with its ID,Name, & price.
             System.out.printf("id: %d %s - Price: $%.2f%n",
                     p.getId(), p.getName(), p.getPrice());
         }
     }
+    //Create a method called getInventory.
+    // this method loads product objects into inventory(new ArrayList)
+    // and its details are not shown = we're also instantiating an object called inventoryProduct.
+    //we add these products to the inventory list.
     public static ArrayList<Product> getInventory() {
         //we're creating a new object called inventory that will contain a list of products.
         ArrayList<Product> inventory = new ArrayList<>();
-        //String[] cheese = {"pig", "dog", "cat"};
-
-        // this method loads product objects into inventory
-        // and its details are not shown= we're also instantiating an object called product.
-        //we add these products to the inventory list.
-        //add that product to the inventory array list using inventory.add
-
         try {
             //open csv file
             FileReader myInventoryFile = new FileReader("src/main/resources/inventory.csv");//opening the file
             BufferedReader myBufferInventoryFile = new BufferedReader(myInventoryFile);//reading the file
 
-            //create a loop //%n USE THIS TO PUT NEXT LINE
+            //create a loop
             String theLine;
             while ((theLine = myBufferInventoryFile.readLine()) != null) {
-                //create a product by splitting the line on the |
-
                 //created a product using the product class and pieces of the string .split("\\|")
                 String[] inventoryDetailInfo = theLine.split("\\|");
-
+                //If the line start with id skip it to read the next line.
                 if (inventoryDetailInfo[0].equalsIgnoreCase("id")) {
                     continue;
                 }
@@ -50,20 +47,16 @@ public class StoreApp {
                 String name = inventoryDetailInfo[1];
                 float price = Float.parseFloat(inventoryDetailInfo[2]);
 
-
-                //instantiate the product object and pass in argument.
-
+                //instantiate a new InventoryProduct object from product class and pass in argument.
                 Product inventoryProduct = new Product(id, name, price);
+                //add that product to the inventory array list using inventory.add
                 inventory.add(inventoryProduct);
             }
             //Close the buffer.
             myBufferInventoryFile.close();
-
         } catch (Exception e) {
             System.out.print("Error reading inventory file." + e.getMessage());
         }
-
         return inventory;
-
     }
 }
